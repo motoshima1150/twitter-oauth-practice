4a0b90f5cf23575ccda693906859bb635ab79af3
motoshima1150 <motonari.nakashima@gmail.com>
2020-05-21 23:16:57 +0900

    Revent

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'home#top'
+>>>>>>> parent of babb447... Reset code
 end
